eb609ba349826a355f58365dd7971aab894ca2bd
Jestan <jestanbrar@gmail.com>
2021-11-07 18:47:30 -0500

    all the board has the necessary text displayed

--- src/MonopolyFrame.java
+++ src/MonopolyFrame.java
@@ -77,6 +77,25 @@ public class MonopolyFrame extends JFrame implements MonopolyView{
             JLabel pName = new JLabel(temp);
             pName.setFont(new Font("Verdana",1,11));
             spaces[0][j].add(pName);
+            if(model.getBoard().get(j) instanceof Event){
+                if(((Event) model.getBoard().get(j)).getPayment() != 0){
+                    JLabel pay = new JLabel("Payment: " + ((Event) model.getBoard().get(j)).getPayment());
+                    pay.setFont(new Font("Verdana",1,10));
+                    spaces[0][j].add(pay);
+                }
+
+            }
+
+
+            if(model.getBoard().get(j) instanceof Railroad){
+                JLabel rent1 = new JLabel("Rent: 25, 50, 100, 200");
+                JLabel price = new JLabel("Price: 200");
+                rent1.setFont(new Font("Verdana",1,10));
+                price.setFont(new Font("Verdana",1,10));
+                spaces[0][j].add(rent1);
+                spaces[0][j].add(price);
+            }
+
             if(model.getBoard().get(j) instanceof Property){
                 JLabel price = new JLabel("Price:  "+ String.valueOf(((Property) model.getBoard().get(j)).getCost()));
                 JLabel rent = new JLabel("Rent: "+ String.valueOf(((Property) model.getBoard().get(j)).getRent()));
@@ -98,6 +117,26 @@ public class MonopolyFrame extends JFrame implements MonopolyView{
             JLabel pName = new JLabel(temp);
             pName.setFont(new Font("Verdana",1,11));
             spaces[10][j].add(pName);
+
+
+            if(model.getBoard().get(count2) instanceof Utilities){
+                JLabel rent1 = new JLabel("Dice Multiplier: 4x, 10x");
+                JLabel price = new JLabel("Price: 150");
+                rent1.setFont(new Font("Verdana",1,10));
+                price.setFont(new Font("Verdana",1,10));
+                spaces[10][j].add(rent1);
+                spaces[10][j].add(price);
+            }
+
+            if(model.getBoard().get(count2) instanceof Railroad){
+                JLabel rent1 = new JLabel("Rent: 25, 50, 100, 200");
+                JLabel price = new JLabel("Price: 200");
+                rent1.setFont(new Font("Verdana",1,10));
+                price.setFont(new Font("Verdana",1,10));
+                spaces[10][j].add(rent1);
+                spaces[10][j].add(price);
+            }
+
             if(model.getBoard().get(count2) instanceof Property){
                 JLabel price = new JLabel("Price:  "+ String.valueOf(((Property) model.getBoard().get(count2)).getCost()));
                 JLabel rent = new JLabel("Rent: "+ String.valueOf(((Property) model.getBoard().get(count2)).getRent()));
@@ -118,6 +157,25 @@ public class MonopolyFrame extends JFrame implements MonopolyView{
             JLabel pName = new JLabel(temp);
             pName.setFont(new Font("Verdana",1,11));
             spaces[i][0].add(pName);
+
+            if(model.getBoard().get(count3) instanceof Event){
+                if(((Event) model.getBoard().get(count3)).getPayment() != 0){
+                    JLabel pay = new JLabel("Payment: " + ((Event) model.getBoard().get(count3)).getPayment());
+                    pay.setFont(new Font("Verdana",1,10));
+                    spaces[i][0].add(pay);
+                }
+
+            }
+
+            if(model.getBoard().get(count3) instanceof Railroad){
+                JLabel rent1 = new JLabel("Rent: 25, 50, 100, 200");
+                JLabel price = new JLabel("Price: 200");
+                rent1.setFont(new Font("Verdana",1,10));
+                price.setFont(new Font("Verdana",1,10));
+                spaces[i][0].add(rent1);
+                spaces[i][0].add(price);
+            }
+
             if(model.getBoard().get(count3) instanceof Property){
                 JLabel price = new JLabel("Price:  "+ String.valueOf(((Property) model.getBoard().get(count3)).getCost()));
                 JLabel rent = new JLabel("Rent: "+ String.valueOf(((Property) model.getBoard().get(count3)).getRent()));
@@ -138,6 +196,24 @@ public class MonopolyFrame extends JFrame implements MonopolyView{
             JLabel a = new JLabel("<html>" + temp + "</html>");
             a.setFont(new Font("Verdana",1,11));
             spaces[i][10].add(a);
+            if(model.getBoard().get(count) instanceof Utilities){
+                JLabel rent1 = new JLabel("Dice Multiplier: 4x, 10x");
+                JLabel price = new JLabel("Price: 150");
+                rent1.setFont(new Font("Verdana",1,10));
+                price.setFont(new Font("Verdana",1,10));
+                spaces[i][10].add(rent1);
+                spaces[i][10].add(price);
+            }
+
+
+            if(model.getBoard().get(count) instanceof Railroad){
+                JLabel rent1 = new JLabel("Rent: 25, 50, 100, 200");
+                JLabel price = new JLabel("Price: 200");
+                rent1.setFont(new Font("Verdana",1,10));
+                price.setFont(new Font("Verdana",1,10));
+                spaces[i][10].add(rent1);
+                spaces[i][10].add(price);
+            }
 
             if(model.getBoard().get(count) instanceof Property){
                 JLabel price = new JLabel("Price:  "+ String.valueOf(((Property) model.getBoard().get(count)).getCost()));
